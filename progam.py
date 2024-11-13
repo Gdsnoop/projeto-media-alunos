69a4ddf225bbc19fe75659266b4372b591ecb95e
Gdsnoop <gustavo.nascimento189@etec.sp.gov.br>
2024-11-13 10:57:45 -0300

    Refatorando o código
    onde a primeira e segunda nota tem peso 1 e a terceira nota peso 2

--- progam.py
+++ progam.py
@@ -2,7 +2,7 @@ nota1 = int(input("Digite a primeira nota "))
 nota2 = int(input("Digite a segunda nota "))
 nota3 = int(input("Digite a terceira nota "))
 
-med = (nota1 + nota2 + nota3) / 3
+med = (nota1 * 1 + nota2 * 1 + nota3 * 2) / (1 + 1 + 2)
 
 print(f"Média é: ", med)
 
